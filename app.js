f738b76d81a850a1e320b9b9add991409c58971c
Jarrett Samuels <jarrettsamuels@Zangestu.local>
2019-02-13 04:45:58 -0500

    replaced two stateless functions with classes; added state to make page dynamic, so can remove players and update score in real time; players counter on scoreboard header now updates to show number of players

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-const players = [
-    {
-    name: "Jay",
-    score: 50,
-    id: 1
-  },
-  {
-    name: "Logan",
-    score: 85,
-    id: 2
-  },
-  {
-    name: "Flash",
-    score: 95,
-    id: 3
-  },
-  {
-    name: "Gambit",
-    score: 80,
-    id: 4
-  }
-]
-
 const Header = (props) =>
         <header>
             <h1>{ props.title }</h1>
@@ -30,28 +7,75 @@ const Header = (props) =>
 const Player = (props) => 
         <div className="player">
             <span className="player-name">
+            <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
                 { props.name }
             </span>
-            <Counter score={ props.score }/>
+            <Counter />
         </div>
 
-const Counter = (props) =>
-    <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score"> { props.score } </span>
-        <button className="counter-action increment"> + </button>
-    </div>
+class Counter extends React.Component {
 
-const App = (props) =>
-    <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={ props.initialPlayers.length } />
+state = {
+    score: 0
+}
+
+incrementScore = () => {
+    this.setState( prevState => ({score: prevState.score + 1}))
+}
+
+decrementScore = () => {
+    this.setState( prevState => ({score: prevState.score - 1}))
+}
+
+render() {
+    return <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score"> { this.state.score } </span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+    </div>
+    }
+}
+
+class App extends React.Component {
+
+    state = {
+        players: [
+            {
+            name: "Jay",
+            id: 1
+          },
+          {
+            name: "Logan",
+            id: 2
+          },
+          {
+            name: "Flash",
+            id: 3
+          },
+          {
+            name: "Gambit",
+            id: 4
+          }
+        ]
+    }
+
+handleRemovePlayer = (id) => {
+    this.setState( prevState => ({players: prevState.players.filter( p => p.id !== id )}))
+}
+
+render() {
+    return <div className="scoreboard">
+        <Header title="Scoreboard" totalPlayers={ this.state.players.length } />
 
         {/* Players list*/}
 
-        {props.initialPlayers.map( player => <Player name={ player.name } score={ player.score } key={ player.id.toString() } /> )}
-    </div>
+        {this.state.players.map( player => <Player name={ player.name } id={player.id} key={ player.id.toString() } removePlayer={this.handleRemovePlayer} /> )}
+            </div>
+    }
+}
+
 
 ReactDOM.render(
-    <App initialPlayers={ players }/>,
+    <App />,
     document.getElementById('root')
 )
